2092da88d551f36a086f67602960f10e160f52d3
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2024-08-29 11:08:53 -0600

    Agregando clase del dia de hoy

---
+++ Clases/29082024.py
@@ -0,0 +1,15 @@
+# Clase del 29 de Agosto del 2024
+import random
+import time
+
+n = 10
+lista = []
+random.random()
+
+
+#Intercambiar es mas facil en Python
+a=temp
+a=b     #A la antiguita
+b=temp
+
+a,b=b,a #Como se hace en Python
