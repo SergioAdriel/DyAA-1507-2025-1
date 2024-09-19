8b170a06b70a690988025509fe805f4dd1b93222
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2024-09-18 18:06:52 -0600

    Agregando Tarea

--- Tareas/Tarea2/Tarea2.py
+++ Tareas/Tarea2/Tarea2.py
@@ -1,16 +1,18 @@
 def carga_palabras():  # Carga palabras desde el archivo
-    with open('C:/Users/Adrie/OneDrive/Escritorio/GitHub/DyAA-1507-2025-1/Tareas/Tarea2/words.txt', 'r') as archivo:
-        palabras = archivo.read().split()
+        archivo = open('C:/Users/Adrie/OneDrive/Escritorio/GitHub/DyAA-1507-2025-1/Tareas/Tarea2/words.txt', 'r')
+        renglon = archivo.readline()
+        palabras = renglon.split()
         print(len(palabras), 'palabras leídas')  # Muestra la cantidad de palabras
-    return palabras
+        return palabras
 
 def carga_cifrado():  # Carga texto cifrado desde el archivo
-    with open('C:/Users/Adrie/OneDrive/Escritorio/GitHub/DyAA-1507-2025-1/Tareas/Tarea2/textoCifrado.txt', 'r') as archivo:
-        return archivo.readline().strip()  # Elimina espacios en blanco
+        archivo = open('C:/Users/Adrie/OneDrive/Escritorio/GitHub/DyAA-1507-2025-1/Tareas/Tarea2/textoCifrado.txt', 'r')
+        renglon = archivo.readline()
+        return renglon
 
 def cifra_cesar(cadena, llave):  # Cifra texto usando el cifrado César
-    if llave < 0:  # Ajusta llave si es negativa
-        llave = 26 - (abs(llave) % 26)  # Se asegura que sea positiva
+    if llave < 0:
+        llave = 26 - llave
     nuevaCadena = ""
     alfabeto = 'abcdefghijklmnopqrstuvwxyz'
     for l in cadena:  # Recorre cada letra
@@ -22,24 +24,25 @@ def cifra_cesar(cadena, llave):  # Cifra texto usando el cifrado César
     return nuevaCadena
 
 def descifra_cesar(cadena, llave):  # Descifra usando el cifrado César
-    return cifra_cesar(cadena, 26 - llave)
+    return cifra_cesar(cadena, 26 -llave)
 
 def get_aciertos(listaPalabras, diccionario):  # Obtiene número de aciertos
     return sum(1 for palabra in listaPalabras if palabra in diccionario)  # Cuenta aciertos
 
-dic1 = carga_palabras()  # Carga el diccionario de palabras
-max_aciertos = 0  # Inicializa max_aciertos
-mejor_llave = 0  # Inicializa mejor_llave
+palabras = carga_palabras()  # Carga el diccionario de palabras
 cadena = carga_cifrado()  # Carga el texto cifrado
+maxAciertos = 0  # Inicializa maxAciertos en 0
+posibleLlave = 0  # Inicializa posibleLLave en 0
+
 for cont in range(26):  # Prueba todas las llaves
     cadenaCifrada = descifra_cesar(cadena, cont)  # Descifra con la llave
     listaCifrada = cadenaCifrada.split()  # Divide el texto en palabras
-    numAciertos = get_aciertos(listaCifrada, dic1)  # Cuenta aciertos
-    if numAciertos > max_aciertos:  # Si hay más aciertos
-        max_aciertos = numAciertos
-        mejor_llave = cont  # Guarda mejor llave
+    numAciertos = get_aciertos(listaCifrada, palabras)  # Cuenta aciertos
+    if numAciertos > maxAciertos:  # Si hay más aciertos
+        maxAciertos = numAciertos
+        posibleLlave = 26 - cont  # Guarda mejor llave
 
-print("La mejor llave es:", 26 - mejor_llave)  # Muestra la mejor llave
+print("La mejor llave es:", posibleLlave)  # Muestra la mejor llave
 print("\nEl texto descifrado es:")
-print(descifra_cesar(cadena, mejor_llave))  # Muestra texto descifrado
+print(cifra_cesar(cadena, posibleLlave))  # Muestra texto descifrado
 print("\n\tCheco estuvo aquí")  # Easter Egg
